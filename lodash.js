9cdb6bd50fc9d60f51af55a4db632bffc957893f
ConstructAware <lp.lencone@gmail.com>
2022-03-22 12:39:57 -0300

    some changes lol

--- lodash.js
+++ lodash.js
@@ -4,3 +4,7 @@ const items = ['banana', 'apple', 'lemon', 'orange']
 
 const itemslol = _.chunk(items, 2)
 console.log(itemslol)
+
+const numbers = [1, [2, [3, [4]]]]
+const newNumbers = _.flattenDeep(numbers)
+console.log(newNumbers)
